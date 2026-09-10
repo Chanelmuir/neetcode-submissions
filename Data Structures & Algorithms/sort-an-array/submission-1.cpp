0dbe9b4fc873263c1d9d83795428127e0a8c886b
ChanelMuir <86148248+Chanelmuir@users.noreply.github.com>
2026-09-10 14:15:13 +1200

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        size_t n = nums.size();
+        if (n <= 1) return nums;
+
+        vector<int> left(nums.begin(), nums.begin() + n/2);
+        vector<int> right(nums.begin() + n/2, nums.end());
+        left  = sortArray(left);
+        right = sortArray(right);
+
+        return merge(left, right);
+    }
+
+    private:
+    vector<int> merge(vector<int>& left, vector<int>& right) {
+        size_t size = left.size() + right.size();
+        vector<int> ans(size);
+        int lpos {0};
+        int rpos {0};
+
+        while(lpos < left.size() && rpos < right.size()) {
+            if (left[lpos] > right[rpos]) {
+                ans[lpos + rpos] = right[rpos];
+                rpos++;
+
+            } else {
+                ans[lpos + rpos] = left[lpos];
+                lpos++;
+            }
+        }
+        while (rpos < right.size()) {
+            ans[rpos + lpos] = right[rpos];
+            rpos++;
+        }
+    
+        while (lpos < left.size()) {
+            ans[rpos + lpos] = left[lpos];
+            lpos++;
+        }
+        return ans;
+    }
+};
