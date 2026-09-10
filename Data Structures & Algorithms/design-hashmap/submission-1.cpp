1f75dcc0cb28f274d1c324b53abbc24de5f8fd40
ChanelMuir <86148248+Chanelmuir@users.noreply.github.com>
2026-09-10 12:17:19 +1200

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.cpp
@@ -0,0 +1,71 @@
+class MyHashMap {
+private:
+    struct Node {
+        int key;
+        int value;
+        Node* next;
+        Node(int k, int v) : key(k), value(v), next(nullptr) {}
+    };
+
+    vector<Node*> hashMap{769};
+
+    int hash(int key) {
+        return key % hashMap.size();
+    }
+
+public:
+    MyHashMap() {
+        for (auto& bucket : hashMap) {
+            bucket = new Node(0, 0);
+        }
+    }
+    
+    void put(int key, int value) {
+        Node* cur = hashMap[hash(key)];
+
+        while(cur->next != nullptr) {
+            if(cur->next->key == key) {
+                cur->next->value = value;
+                return;
+            }
+            cur = cur->next;
+        }
+        cur->next = new Node(key, value);
+        return;
+    }
+    
+    int get(int key) {
+        Node* cur = hashMap[hash(key)];
+
+        while(cur->next != nullptr) {
+            if(cur->next->key == key) {
+                return cur->next->value;
+            }
+            cur = cur->next;
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        Node* cur = hashMap[hash(key)];
+
+        while(cur->next != nullptr) {
+            if(cur->next->key == key) {
+                Node* toDelete = cur->next;
+                cur->next = toDelete->next;
+                delete toDelete;
+                return;
+            }
+            cur = cur->next;
+        }
+        return;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
