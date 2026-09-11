49124b6d5a3bd95947ee2c5d9753fb30d14bb7e0
ChanelMuir <86148248+Chanelmuir@users.noreply.github.com>
2026-09-11 14:52:41 +1200

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int size = s.size();
+        int i {0};
+        int j {size - 1};
+        while (i < j) {
+            char left = s[i];
+            char right = s[j];
+
+            if (left >= 'A' && left <= 'Z') {left += 32;}
+            if (right >= 'A' && right <= 'Z') {right += 32;}
+
+            if( left >= '0' && left <= '9' ||
+                left >= 'a' && left <= 'z'
+                ) {
+                    if (right >= '0' && right <= '9' ||
+                        right >= 'a' && right <= 'z'
+                        ) {
+                            if (left == right) {
+                                ++i; --j;
+                            } else {
+                                return false;
+                            }
+                        } else {
+                            --j;
+                        }
+                    
+                } else {
+                    ++i;
+                }
+        }
+        return true;
+    }
+};
